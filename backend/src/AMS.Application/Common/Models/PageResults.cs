97604f511fb988d44ab72f7c713f8133a5c98f7c
Momin Sheikh <momincse13@gmail.com>
2026-08-05 20:46:07 +0600

    Pagination Start but pose

---
+++ backend/src/AMS.Application/Common/Models/PageResults.cs
@@ -0,0 +1,21 @@
+namespace AMS.Application.Common.Models;
+
+public class PageResults<T>
+{
+    public IReadOnlyList<T> Items { get; set; } = [];
+
+    public int PageNumber { get; set; }
+
+    public int PageSize { get; set; }
+
+    public int TotalCount { get; set; }
+
+    public int TotalPages =>
+        (int)Math.Ceiling((double)TotalCount / PageSize);
+
+    public bool HasPreviousPage =>
+        PageNumber > 1;
+
+    public bool HasNextPage =>
+        PageNumber < TotalPages;
+}
